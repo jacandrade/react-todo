58d6abc95dcb6f64c07e8efd0034094d5bfe1743
Jorge Andrade <jacandrade@gmail.com>
2016-12-25 23:06:25 -0200

    complete handleAddTodo

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -7,7 +7,16 @@ var TestUtils = require('react-addons-test-utils');
 var TodoApp = require('TodoApp');
 
 describe('TodoApp', () => {
+    
     it('should exist', () => {
         expect(TodoApp).toExist();
+    });
+
+    it('should add todo to the todos state on handleAddTodo', () => {
+        var todoText = 'Study more!';
+        var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+        todoApp.setState({todos: []});
+        todoApp.handleAddTodo(todoText);
+        expect(todoApp.state.todos[0].text).toBe(todoText);
     });
 });
