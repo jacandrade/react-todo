56b564a8ebe9f6d4218747943a17a8140f9cdf66
Jorge Andrade <jacandrade@gmail.com>
2016-12-27 11:04:31 -0200

    Added filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -26,7 +26,22 @@ module.exports = {
         filteredTodos = filteredTodos.filter((todo) => {
             return !todo.completed || completed;
         });
+        
+        filteredTodos = filteredTodos.filter((todo) => {
+            
+            return searchText.length === 0 || todo.text.toLowerCase().indexOf(searchText) > -1;
+        });
 
+        filteredTodos = filteredTodos.sort((a, b) => {
+            
+            if(!a.completed && b.completed) {
+                return -1;
+            }else if(a.completed && !b.completed) {
+                return 1;
+            }else {
+                return 0;
+            }
+        });
         return filteredTodos;
     }
 };
--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -42,7 +42,7 @@ describe('TodoAPI', () => {
 
 
     describe('filterTodos', () => {
-        var todos = [{id:1, text:'testing component 1', completed: true}, 
+        var todos = [{id:1, text:'component 1', completed: true}, 
         {id:2, text:'testing component 2', completed: false},
         {id:3, text:'testing component 3', completed: true}
         ];
@@ -54,6 +54,20 @@ describe('TodoAPI', () => {
         it('should return only NOT completed items if showcompleted is false', () => {
             var filteredTodos = TodoAPI.filterTodos(todos, false, '');
             expect(filteredTodos.length).toBe(1);
+        });
+        it('should sort by completed status', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+            expect(filteredTodos[0].completed).toBe(false);
+        });
+
+        it('should filter todos by searchText', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true, 'testing');
+            expect(filteredTodos.length).toBe(2);            
+        });
+
+        it('should return all todos if searchText is empty', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+            expect(filteredTodos.length).toBe(3);            
         });
 
     });
