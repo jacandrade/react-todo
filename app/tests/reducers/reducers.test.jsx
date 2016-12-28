6b8e4d3ecf4ec53a7151f09201ec8c4e3ec5fdc3
Jorge Andrade <jacandrade@gmail.com>
2016-12-28 10:32:29 -0200

    Added todoReducers and respective tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -28,4 +28,39 @@ describe('Reducers', () => {
             expect(res).toEqual(false);
         });
     });
+
+    describe('todosReducer', () => {
+        it('should add new todo', () => {
+            
+            var action = {
+                type: 'ADD_TODO',
+                text: 'some string'
+            };
+
+            var res = Reducers.todosReducer(df([]), df(action));
+
+            expect(res.length).toEqual(1);
+            expect(res[0].text).toEqual(action.text);
+        });
+
+        it('should toggle completed when given id', () => {
+            
+            var state = [{
+                   id: 1,
+                   text: 'test',
+                   completed: true,
+                   createdAt: 123,
+                   completedAt: 124
+            }];
+            var action = {
+                type: 'TOGGLE_TODO',
+                id: 1
+            };
+
+            var res = Reducers.todosReducer(df(state), df(action));
+
+            expect(res[0].completedAt).toEqual(undefined);
+            expect(res[0].completed).toEqual(false);
+        });
+    });
 });
