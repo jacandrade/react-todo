671337331c574517e4efb37f0178fddbdd210615
Jorge Andrade <jacandrade@gmail.com>
2016-12-30 15:32:13 -0200

    Updated db structure to include user and private todos

--- app/actions/actions.jsx
+++ app/actions/actions.jsx
@@ -23,8 +23,9 @@ export var startAddTodo = (text) => {
             createdAt: moment().unix(),
             completedAt: null
         };
+        var uid = getState().auth.uid;
 
-        var todoRef = firebaseRef.child('todos').push(todo);
+        var todoRef = firebaseRef.child(`users/${uid}/todos`).push(todo);
         return todoRef.then(() => {
             dispatch(addTodo({
                 ...todo,
@@ -43,7 +44,8 @@ export var addTodos = (todos) => {
 
 export var startAddTodos = () => {
     return (dispatch, getState) => {
-        var todosRef = firebaseRef.child('todos');
+        var uid = getState().auth.uid;
+        var todosRef = firebaseRef.child(`users/${uid}/todos`);
     
         return todosRef.once('value').then((snapshot) => {
             var todos = snapshot.val() || {};
@@ -78,7 +80,8 @@ export var updateTodo = (id, updates) => {
 
 export var startToggleTodo = (id, completed) => {
     return (dispatch, getState) => {
-        var todoRef = firebaseRef.child(`todos/${id}`);
+        var uid = getState().auth.uid;
+        var todoRef = firebaseRef.child(`users/${uid}/todos/${id}`);
         var updates = {
             completed, 
             completedAt: completed ? moment().unix() : null
